46b3fe085ed6eeb7af24e29e8af29ba3cc4b527a
unknown <narmada.vaddireddy97@gmail.com>
2023-03-31 12:52:20 +0530

    Added: Code to create employee payroll service database in curd Operation

--- Ms-Sql-Db.sql
+++ Ms-Sql-Db.sql
@@ -12,4 +12,9 @@ start DATE not null,
 primary key (id)
 );
 describe employee_payroll;
+#UC3
+insert into employee_payroll(name, salary, start)values
+('Bill',1000000.00,'2018-01-03'),
+('Terisa',2000000.00,'2020-11-13'),
+('Charlie',4000000.00,'2020-05-21');
 
