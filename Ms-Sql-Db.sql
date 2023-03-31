b1f15fe3696f077bbd0c221de8907b056bb60c3f
unknown <narmada.vaddireddy97@gmail.com>
2023-03-31 12:45:40 +0530

    Added: Code create a employee payroll table

--- Ms-Sql-Db.sql
+++ Ms-Sql-Db.sql
@@ -2,4 +2,14 @@
 create database payroll_service;
 show databases;
 use payroll_service;
+#UC2
+create Table employee_payroll
+(
+id INT unsigned not null auto_increment,
+name varchar(150) not null,
+salary double not null,
+start DATE not null,
+primary key (id)
+);
+describe employee_payroll;
 
